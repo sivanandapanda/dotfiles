4a37415a0436c3aaffcefbe8c48af31e1d5f1a4f
Ubuntu <maxx@ubuntucloudxfce.k0cksp04bcoujoyuam3h2nwavg.ix.internal.cloudapp.net>
2021-06-23 16:56:05 +0000

    added ubuntu setup script

---
+++ setup.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+sudo apt-get update -y
+
+sudo apt-get upgrade -y
+
+#intellij
+
+sudo snap install intellij-idea-community --classic
+
+#jdk
+
+sudo apt install openjdk-16-jdk -y
+
+#docker
+
+sudo apt-get install apt-transport-https ca-certificates curl gnupg lsb-release
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
+echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt-get update
+
+sudo apt-get install docker-ce docker-ce-cli containerd.io -y
+
+sudo groupadd docker
+
+sudo usermod -aG docker $USER
+
+newgrp docker
+
+docker run hello-world
+
+#vs code
+
+sudo snap install --classic code
+
+#maven
+
+sudo apt-get install maven -y
+
+#chrome
+
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+
+sudo apt install ./google-chrome-stable_current_amd64.deb
+
+#direnv
+
+sudo apt install direnv
